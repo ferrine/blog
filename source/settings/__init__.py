1c30a62abf216f2c549ce6cfd20ad60ae2237a17
Max Kochurov <maxim.v.kochurov@gmail.com>
2023-05-18 18:39:32 +0300

    add local links to gitignore

--- source/settings/__init__.py
+++ source/settings/__init__.py
@@ -2,3 +2,4 @@ from .ablog import *
 from .carousel import *
 from .html import *
 from .nbsphinx import *
+from .links import *
---
+++ source/settings/links.py
@@ -0,0 +1,8 @@
+# https://github.com/sphinx-doc/sphinx/issues/5208
+linkcheck_ignore = [
+    "cases/bioinfo.html",
+    "cases/ab.html",
+    "cases/chemistry.html",
+    "cases/mmm.html",
+    "cases/brain.html",
+]

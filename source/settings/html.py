cf2c263818c8b190e26b9cbc6cb7fb4dce5f5ef1
Max Kochurov <maxim.v.kochurov@gmail.com>
2023-05-18 12:06:02 +0300

    split stylesheets

--- source/settings/html.py
+++ source/settings/html.py
@@ -6,7 +6,9 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_static_path = ["_static"]
 html_css_files = [
-    "css/custom.css",
+    "css/light/default.css",
+    "css/dark/default.css",
+    "css/font/default.css",
 ]
 html_favicon = "_static/favicon.ico"
 html_theme = "pydata_sphinx_theme"

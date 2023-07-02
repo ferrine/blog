235f4a0432f126e205ad0c4c6ce50b09b75fb03b
Max Kochurov <maxim.v.kochurov@gmail.com>
2023-07-02 13:18:04 +0000

    make everything shiny

--- source/settings/nbsphinx.py
+++ source/settings/nbsphinx.py
@@ -2,11 +2,16 @@ nbsphinx_execute = "never"
 nbsphinx_input_prompt = "%.0s"
 nbsphinx_output_prompt = "%.0s"
 nbsphinx_prompt_width = "0"
-nbsphinx_requirejs_path = ""
-nbsphinx_requirejs_options = {}
 nbsphinx_epilog = """
 ----
 
-If you have new ideas after reading the notebook and want to discuss it in more details,
+If you have new ideas after reading the notebook, 
+want to discuss it in more details or want to work with me,
 you can always :doc:`reach me out </contacts>`.
+
+.. dropdown:: Resources
+    :animate: fade-in-slide-down
+
+    * Download the :download:`post.ipynb`
+    * Download the :download:`environment.yml`
 """

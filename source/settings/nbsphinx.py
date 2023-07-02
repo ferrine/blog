e3d7f1ec18db288442f7acb7fdcf87a4b375c8f4
Max Kochurov <maxim.v.kochurov@gmail.com>
2023-07-02 12:34:07 +0000

    no telegram contact

--- source/settings/nbsphinx.py
+++ source/settings/nbsphinx.py
@@ -1,10 +1,12 @@
 nbsphinx_execute = "never"
 nbsphinx_input_prompt = "%.0s"
 nbsphinx_output_prompt = "%.0s"
+nbsphinx_prompt_width = "0"
 nbsphinx_requirejs_path = ""
 nbsphinx_requirejs_options = {}
 nbsphinx_epilog = """
----
-If you have new ideas after reading the notebook and want to discuss it more detailed, 
-you can always :doc:`reach me out </contacts>`
+----
+
+If you have new ideas after reading the notebook and want to discuss it in more details,
+you can always :doc:`reach me out </contacts>`.
 """
